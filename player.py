36213d497e14579bd244acf45976bced816485e9
Andrej <andrejtodoroski@yahoo.com>
2024-11-25 20:48:27 -0500

    fixed the player import issue and character movement

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from setting import * 
+from settings import * 
 import pygame as pg
 import math
 
@@ -11,7 +11,7 @@ class Player:
     def movement(self):
         sin_a = math.sin(self.angle)
         cos_a = math.cos(self.angle)
-        dx, dy = 0,0
+        dx, dy = 0, 0
         speed = PLAYER_SPEED * self.game.delta_time
         speed_sin = speed * sin_a
         speed_cos = speed * cos_a
@@ -24,23 +24,23 @@ class Player:
             dx += -speed_cos
             dy += -speed_sin
         if keys[pg.K_a]:
-            dy += speed_sin
-            dx += -speed_cos
+            dx += speed_sin
+            dy += -speed_cos
         if keys[pg.K_d]:
-            dy += -speed_sin
-            dx += speed_cos
+            dx += -speed_sin
+            dy += speed_cos
 
         self.x += dx
         self.y += dy
 
-        if [keys[pg.K_LEFT]]:
+        if keys[pg.K_LEFT]:
             self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
-        if [keys[pg.K_RIGHT]]:
+        if keys[pg.K_RIGHT]:
             self.angle += PLAYER_ROT_SPEED * self.game.delta_time
         self.angle %= math.tau
 
     def draw(self):
-        pg.draw_line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),(
+        pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),(
             self.x * 100 + WIDTH * math.cos(self.angle),
             self.y * 100 + HEIGHT * math.sin(self.angle)), 2)
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
